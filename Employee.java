6b975a1c8f469bf2d90abcfa2481019f1cf3c147
Ayesha Akter <113846939+ariyaayesha@users.noreply.github.com>
2023-02-13 00:08:13 +0600

    parameter constructor

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    int id;
+    String name;
+
+    public Employee(int a, String name) {
+        this.id = a;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+        Employee myObj = new Employee(5317,"ayesha");
+        System.out.println("My id is" + " "+ myObj.id);
+        System.out.println("My name is"+" "+myObj.name);
+
+
+    }
+}
